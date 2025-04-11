ed9dfb2940635fa68338cae466a4ddc247467e35
opaioa1 <gustavolinsbatista9@gmail.com>
2025-04-10 22:43:25 -0300

    Update a. main.py

--- a. main.py
+++ a. main.py
@@ -13,3 +13,15 @@ def main():
 
 if name == 'main':
     main()
+b.config
+TOKEN = '7875219149:AAEp8kmi0gPDgXop3QTxUlLO3IFq-bmv4bA'
+c. requirements.txt
+python-telegram-bot==13.15
+d. Procfile
+worker: python a.main.py
+
+git clone https://github.com/opaioa1/sextafeira-futebol-bot.git
+cd sextafeira-futebol-bot
+git add . 
+git commit -m "Configuração inicial do bot"
+git push origin main
